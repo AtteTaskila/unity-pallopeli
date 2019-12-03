9158714ba84d6790258936896e9c4d8c1159b077
Atte Taskila <attetaskila1@hotmail.fi>
2019-12-03 12:07:08 +0200

    Initial commit

---
+++ Assets/Scenes/PelaajakoodiAzzom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelaajakoodiAzzom : MonoBehaviour
+public class KameraKoodi : MonoBehaviour
+{
+    public float nopeus;
+    public Transform PelaajakoodiAzzom;
+    Vector3 suunta;
+    Rigidbody RB;
+    // Start is called before the first frame update
+    void Start(){
+       RB = GetComponent <Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        suunta.x = Input.GetAxis("Horizontal");
+        suunta.z = Input.GetAxis("Vertical");
+        RB.AddForce(suunta * nopeus);
+    }
+}
